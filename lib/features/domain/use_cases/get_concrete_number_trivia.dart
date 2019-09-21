27aa46b522de208826f23ab275fe18ce87affc22
manuelvargastapia <manuelvargastapia@gmail.com>
2019-09-21 13:04:49 -0300

    Refactor Use case class to implement UseCase abstract class with Params class

--- lib/features/domain/use_cases/get_concrete_number_trivia.dart
+++ lib/features/domain/use_cases/get_concrete_number_trivia.dart
@@ -1,5 +1,7 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:number_trivia/core/use_cases/use_case.dart';
 
 import '../../../core/error/failure.dart';
 import '../entities/number_trivia.dart';
@@ -7,18 +9,33 @@ import '../repositories/number_trivia_repository.dart';
 
 /// Use Case Class.
 /// It holds one of our use cases: get an Entity (or Failure) from Repository.
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository numberTriviaRepository;
 
   GetConcreteNumberTrivia(this.numberTriviaRepository);
 
   // This method is being implemented here only after we write the test for this class.
-  // In the test, we've defined what is the actual behaviour of this method.
-  // To follow TDD in a strict way, we should run the test without implementing completely this
-  // method. In this case, though, only have one line.
+  //
+  // TDD workflow:
+  // 1. RED (fail the test)
+  // 2. GREEN (pass the test)
+  // 3. REFACTOR (rewrite the code already written from the test and the test itself)
 
-  /// Implementation of Repostiroy method.
-  Future<Either<Failure, NumberTrivia>> execute({@required int number}) async {
-    return await numberTriviaRepository.getConcreteNumberTrivia(number);
-  }
+  // Implementation of Repostiroy method (before refactoring)
+  // Future<Either<Failure, NumberTrivia>> execute({@required int number}) async {
+  //  return await numberTriviaRepository.getConcreteNumberTrivia(number);
+  // }
+
+  // Implementation of Repository method (refactored, using Dart Callable Class
+  // via implementing UseCase abstract class)
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async =>
+      await numberTriviaRepository.getConcreteNumberTrivia(params.number);
+}
+
+/// Params class to hold all the params for call() in abstract UseCase class.
+class Params extends Equatable {
+  final int number;
+
+  Params({@required this.number});
 }
